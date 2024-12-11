e294f8ff4ded114c6394562bcaa671a79900e3f2
Liu Shibo <156430285+liuxiaZzz@users.noreply.github.com>
2024-12-11 21:15:26 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,11 +45,11 @@ html_theme_options = {
 # -- Options for EPUB output
 epub_show_urls = 'footnote' 
 
-html_static_path = ["../../Figs"]
+# html_static_path = ["../Figs"]
 
 nbsphinx_thumbnails = {
-    "../notebooks/run_DIRAC_DBit-seq.ipynb": "../../Figs/tutorial_figs/notebooks_run_DIRAC_DBit-seq_22_0.png",
-    "../notebooks/run_DIRAC_on_DLPFC.ipynb": "../../Figs/tutorial_figs/notebooks_run_DIRAC_on_DLPFC_9_1.png",
-    "../notebooks/run_DIRAC_mouse_spleen_bin100.ipynb": "../../Figs/tutorial_figs/notebooks_run_DIRAC_mouse_spleen_bin100_33_1.png",
-    "../notebooks/run_DIRAC_mouse_spleen_cellbin.ipynb": "../../Figs/tutorial_figs/notebooks_run_DIRAC_mouse_spleen_cellbin_26_2.png",
+    "../notebooks/run_DIRAC_DBit-seq.ipynb": "../Figs/tutorial_figs/notebooks_run_DIRAC_DBit-seq_22_0.png",
+    "../notebooks/run_DIRAC_on_DLPFC.ipynb": "../Figs/tutorial_figs/notebooks_run_DIRAC_on_DLPFC_9_1.png",
+    "../notebooks/run_DIRAC_mouse_spleen_bin100.ipynb": "../Figs/tutorial_figs/notebooks_run_DIRAC_mouse_spleen_bin100_33_1.png",
+    "../notebooks/run_DIRAC_mouse_spleen_cellbin.ipynb": "../Figs/tutorial_figs/notebooks_run_DIRAC_mouse_spleen_cellbin_26_2.png",
 }
