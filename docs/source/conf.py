21cd210083d088e4590b445ffcb9f00c1adff0b3
Liu Shibo <156430285+liuxiaZzz@users.noreply.github.com>
2025-05-22 14:29:05 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 project = 'TEST_DIRAC'
 copyright = '2024, CHANG XU'
