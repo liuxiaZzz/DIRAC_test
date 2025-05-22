000ef2991441c7a23f26a6c3ce1a2a2be592f5da
Liu Shibo <156430285+liuxiaZzz@users.noreply.github.com>
2025-05-22 14:33:51 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
 project = 'TEST_DIRAC'
 copyright = '2024, CHANG XU'
